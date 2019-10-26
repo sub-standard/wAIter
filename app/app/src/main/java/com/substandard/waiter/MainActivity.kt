ba86f817adadeee925ea613cf0bee07adb59b69c
Pav Sidhu <pav@pavsidhu.com>
2019-10-26 22:06:54 +0100

    beacon setup

--- app/app/src/main/java/com/substandard/waiter/MainActivity.kt
+++ app/app/src/main/java/com/substandard/waiter/MainActivity.kt
@@ -2,11 +2,13 @@ package com.substandard.waiter
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.uriio.beacons.Beacons
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        Beacons.initialize(this);
     }
 }
--- app/app/src/main/java/com/substandard/waiter/ui/main/MainViewModel.kt
+++ app/app/src/main/java/com/substandard/waiter/ui/main/MainViewModel.kt
@@ -1,7 +1,16 @@
 package com.substandard.waiter.ui.main
 
 import androidx.lifecycle.ViewModel
+import com.uriio.beacons.model.iBeacon
+
 
 class MainViewModel : ViewModel() {
+    private val id = "abcdefghijklmnop"
 
+    private val beacon =
+        iBeacon(id.toByteArray(), 0, 0, "wAIter")
+
+    fun startBeacon() = beacon.start()
+
+    fun stopBeacon() = beacon.stop()
 }
